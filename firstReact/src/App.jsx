6df6aa753e6d624c9a6d8a6ed4c85b1bf2e4deca
Katlego Dhlamini <katlegodhlamini2003@gmail.com>
2025-10-22 14:54:15 +0200

    adding api

--- firstReact/src/App.jsx
+++ firstReact/src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
+import {use, useEffect, useState } from 'react'
 // import reactLogo from './assets/react.svg'
 
 import './App.css'
 import Search  from './components/search.jsx'
 
+const API_BASE_URL = 'https://api.themoviedb.org/3'
+const API_KEY = import.meta.env.VITE_API_KEY
+
+API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
+
 function App() {
+  useEffect( () => {
+
+  }
+  , [])
 
   const [searchTerm, setSearchTerm] = useState('')
 
